ad4126c6e0e46500b60ed65ab93a123abf6c7ff6
BlazerXXI <ruslandendik@gmail.com>
2023-07-31 23:59:15 +0300

    fix components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import React from "react";
 import "./globals.scss";
-import Header from "./components/header/page";
 
 const page = () => {
-	return (
-		<>
-			<header className="py-6 -mx-2 px-2">
-				<Header />
-			</header>
-			<main></main>
-			<footer></footer>
-		</>
-	);
+	return <main>
+		
+	</main>;
 };
 
 export default page;
