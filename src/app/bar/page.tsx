b1bf2cb7a9c06626ab42f077051aceeee37f8bcd
BlazerXXI <ruslandendik@gmail.com>
2023-08-03 19:52:36 +0300

    dev

--- src/app/bar/page.tsx
+++ src/app/bar/page.tsx
@@ -39,58 +39,63 @@ const Bar = () => {
 		};
 	}, [isZoomed, bar.length]);
 	return (
-		<div>
+		<section id="bar">
 			<div>
-				<h3 className="sub-title-section">Попкорн 🍿</h3>
+				<h2 className="title-section">Бар / кальян / попкорн</h2>
 			</div>
-			<ul className="grid md:grid-cols-2 lg:grid-cols-4 md:mt-12 mt-7 gap-16">
-				{bar.map((item, index) => (
-					<li
-						className="text-center flex flex-col items-center justify-content-center "
-						key={index}
-					>
-						<div className="max-w-[240px] card-border flex flex-col grow gap-5">
-							{item.src ? (
-								<div
-									ref={zoomedImageRef}
-									className="w-[240px] h-[320px] z-20 flex flex-col align-items-center justify-center"
-								>
-									<Image
-										onClick={() => handleImageClick(index)}
-										className={`rounded-lg object-cover ${
-											isZoomed[index] ? "zoomed-image" : ""
-										}`}
-										src={item.src}
-										alt={`блюдо` + " " + item.title}
-										width={240}
-										height={300}
-									/>
+			<div>
+				<div>
+					<h3 className="sub-title-section">Попкорн 🍿</h3>
+				</div>
+				<ul className="grid md:grid-cols-2 lg:grid-cols-4 md:mt-12 mt-7 gap-16">
+					{bar.map((item, index) => (
+						<li
+							className="text-center flex flex-col items-center justify-content-center "
+							key={index}
+						>
+							<div className="max-w-[240px] card-border flex flex-col grow gap-5">
+								{item.src ? (
+									<div
+										ref={zoomedImageRef}
+										className="w-[240px] h-[320px] z-20 flex flex-col align-items-center justify-center"
+									>
+										<Image
+											onClick={() => handleImageClick(index)}
+											className={`rounded-lg object-cover ${
+												isZoomed[index] ? "zoomed-image" : ""
+											}`}
+											src={item.src}
+											alt={`блюдо` + " " + item.title}
+											width={240}
+											height={300}
+										/>
+									</div>
+								) : (
+									<div className="w-[220px] h-[320px] flex flex-col justify-center m-auto">
+										<Image
+											className="rounded-lg m-auto no-photo"
+											src="/img/no_photo.png"
+											width={220}
+											height={220}
+											alt="no photo"
+										/>
+									</div>
+								)}
+								<div className="px-3 pb-4 flex flex-col z-10 grow gap-5">
+									<h4 className="text-[#5e87c9] text-xl md:text-2xl">
+										{item.title}
+									</h4>
+									<p className="opacity-80">{item.description}</p>
+									<p className="flex flex-col grow justify-end text-2xl">
+										{item.price} грн
+									</p>
 								</div>
-							) : (
-								<div className="w-[220px] h-[320px] flex flex-col justify-center m-auto">
-									<Image
-										className="rounded-lg m-auto no-photo"
-										src="/img/no_photo.png"
-										width={220}
-										height={220}
-										alt="no photo"
-									/>
-								</div>
-							)}
-							<div className="px-3 pb-4 flex flex-col z-10 grow gap-5">
-								<h4 className="text-[#5e87c9] text-xl md:text-2xl">
-									{item.title}
-								</h4>
-								<p className="opacity-80">{item.description}</p>
-								<p className="flex flex-col grow justify-end text-2xl">
-									{item.price} грн
-								</p>
 							</div>
-						</div>
-					</li>
-				))}
-			</ul>
-		</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		</section>
 	);
 };
 
