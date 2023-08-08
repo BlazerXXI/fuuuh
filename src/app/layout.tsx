c76d3234d28ff67a482b88c59a57d8732e2f1919
BlazerXXI <103393085+BlazerXXI@users.noreply.github.com>
2023-08-08 16:05:50 +0300

    Update layout.tsx -client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
-"use client";
 import "./globals.scss";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { GetTheme } from "./switchTheme/page";
-import { useState, useEffect } from "react";
 
 const roboto = Roboto({
 	subsets: ["latin"],
@@ -16,35 +13,16 @@ export const metadata: Metadata = {
 	title: "Fuuuh Bar",
 	description: "Fuuuh Bar menu",
 };
-const storedTheme = localStorage.getItem("theme");
-const initialTheme = storedTheme !== null ? storedTheme : "light";
+
+
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const [currentTheme, setCurrentTheme] = useState(initialTheme);
 
-	useEffect(() => {
-		const darkThemeMediaQuery = window.matchMedia(
-			"(prefers-color-scheme: dark)"
-		);
-		const handleThemeChange = (event) => {
-			setCurrentTheme(event.matches ? "dark" : "light");
-		};
-		darkThemeMediaQuery.addListener(handleThemeChange);
-		handleThemeChange(darkThemeMediaQuery);
-		return () => {
-			darkThemeMediaQuery.removeListener(handleThemeChange);
-		};
-	}, []);
-
-	useEffect(() => {
-		localStorage.setItem("theme", currentTheme);
-	}, [currentTheme]);
-
-	const bodyClassNames = `${roboto.className} ${currentTheme}`;
+	const bodyClassNames = `${roboto.className}`;
 	return (
 		<html className="container" lang="uk">
 			<body className={bodyClassNames}>
