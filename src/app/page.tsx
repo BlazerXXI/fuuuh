43a8af62b1e74f96b55d8225d8da871b51f06d6b
BlazerXXI <ruslandendik@gmail.com>
2023-08-10 15:54:52 +0300

    dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,25 @@ import "./globals.scss";
 import Image from "next/image";
 
 const page = () => {
+	const currentTime = new Date().getHours();
+	const isDaytime = currentTime >= 6 && currentTime < 18;
 	return (
-		<section className="mainPage min-h-[100vh] pt-[-56px] flex flex-col justify-start items-center text-center gap-10">
-			<div className="mb-4">
-				<h3 className="text-white text-xl font-bold mt mt-4">
-					Вас вітає Фууух Бар
+		<section className="mainPage min-h-[100vh] pt-[-56px] flex flex-col justify-start items-center text-center gap-6">
+			<div className="mb-4 uppercase flex flex-col gap-2">
+				<h3 className="text-lg opacity-75 font-bold mt mt-4 flex flex-col">
+					{isDaytime ? "Добрий день" : "Добрий вечір"}
+					<br /> Вас вітає
 				</h3>
+				<h2 className="pt-2 mt-2">
+					<span className="text-xl d-flex flex-col font-semibold title-main-menu">
+						Фууух Бар
+					</span>
+				</h2>
 			</div>
 			<div className="mt-4 rounded-lg h-[20vh] min-w-[320px] mainCards ">
 				<Link className="relative" href={"/food"}>
 					<Image
-						className=" object-cover rounded-md m-auto w-[100vw] filter brightness-75 transition-all duration-300"
+						className=" object-cover rounded-md m-auto w-[100vw] filter brightness-75 animateBait transition-all duration-300"
 						src={"/img/salats/more.jpeg"}
 						alt={"food"}
 						width={320}
@@ -32,7 +40,7 @@ const page = () => {
 			<div className="mt-4 rounded-lg h-[20vh] min-w-[320px] mainCards ">
 				<Link className="relative" href={"/bar"}>
 					<Image
-						className=" object-cover rounded-md m-auto w-[100vw] filter brightness-75 transition-all duration-300"
+						className=" object-cover rounded-md m-auto w-[100vw] filter brightness-75 animateBait transition-all duration-300"
 						src={"/img/fuuuhbg.jpeg"}
 						alt={"food"}
 						width={320}
@@ -50,7 +58,7 @@ const page = () => {
 			<div className="mt-4 rounded-lg h-[20vh] min-w-[320px] mainCards ">
 				<Link className="relative" href={"/pets"}>
 					<Image
-						className=" object-cover rounded-md m-auto w-[100vw] filter brightness-75 transition-all duration-300"
+						className=" object-cover rounded-md m-auto w-[100vw] filter brightness-75 animateBait transition-all duration-300"
 						src={"/img/dog.jpg"}
 						alt={"food"}
 						width={320}
